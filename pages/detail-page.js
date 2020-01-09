6b4d02bb4f4773f1bc513933b103470385c64e49
jhhest <janvanhest@outlook.com>
2020-01-09 16:48:45 +0100

    Create elements with data from form

--- pages/detail-page.js
+++ pages/detail-page.js
@@ -1,3 +1,16 @@
 function submitComment() {
-  console.log("Hello");
+  const inputField = document.getElementById("name");
+  const name = inputField.value;
+  const textArea = document.getElementById("msg");
+  const msg = textArea.value;
+  const comment = document.createElement("section");
+  const h3 = document.createElement("h3");
+  h3.innerHTML = `${name} said:`
+  const p = document.createElement("p")
+  p.innerHTML = msg;
+
+  comment.classList.add('comment')
+  comment.appendChild(h3)
+  comment.appendChild(p)
+  console.log(comment);
 }
